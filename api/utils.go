7170b8db49edbd6f2a0162c23d4b3e8ca69bef89
Dave Kettmann <chilini@gmail.com>
2017-06-21 17:46:01 -0400

    Moved from api directory to root

--- api/utils.go
+++
@@ -1,9 +0,0 @@
-package gw2
-
-import "encoding/json"
-
-// PrettyPrint will pretty print any variable in JSON format
-func PrettyPrint(v interface{}) {
-	b, _ := json.MarshalIndent(v, "", "  ")
-	println(string(b))
-}
