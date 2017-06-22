6c23bf6c667dbdff38291b9ba3c14435dedfd375
Dave Kettmann <chilini@gmail.com>
2017-06-22 11:06:42 -0400

    Return errors instead of bailing with log.Fatal()

--- config.go
+++ config.go
@@ -2,19 +2,26 @@ package gw2
 
 import (
 	"encoding/json"
-	"log"
 	"os"
 )
 
+// Config stores the configuration for the application Right now, there are two required objects required:
+// - apiKey: <String> - API key generated in the Account page of guildwars2.com
+// - baseURL: <String> - baseURL to use for API access. Currently only 'https://api.guildwars2.com/'
+type Config struct {
+	APIKey  string `json:"apiKey"`
+	BaseURL string `json:"baseURL"`
+}
+
 // LoadConfig loads the configuration from 'config.json' in the current directory
-func LoadConfig(file string) Config {
+func LoadConfig(file string) (Config, error) {
 	var config Config
 	configFile, err := os.Open(file)
 	defer configFile.Close()
 	if err != nil {
-		log.Println(err.Error())
+		return Config{}, err
 	}
 	jsonParser := json.NewDecoder(configFile)
 	jsonParser.Decode(&config)
-	return config
+	return config, nil
 }
